f4840b564da3911e641a94fa0f1a1956a05a5b38
Rian-Bittencourt <109157383+Rian-Bittencourt@users.noreply.github.com>
2022-07-12 11:59:47 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+// Carne - 400 gr por pessoa   + de 6 horas - 650
+// Cerveja - 1200 ml por Pessoa + 6 horas - 2000 ml
+// Refrigerante/agua - 1000 ml por pessoa + 6 horas 1500ml
+// Crianças valem por 0,5
+
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular() {
+    console.log("Calculando...");
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+
+    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos;
+    let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+
+
+    resultado.innerHTML = `<p class="carne"><img src="/imgs/carne-icon.png" width="30px">${qdtTotalCarne / 1000} Kg de Carne</p>`
+    resultado.innerHTML += `<p class="cerveja"><img src="/imgs/cerveja-icon.png" width="30px">${Math.ceil(qdtTotalCerveja / 355)} Latas de Cerveja</p>`
+    resultado.innerHTML += `<p class="refri"><img src="/imgs/refri-icon.png" width="30px">${Math.ceil(qdtTotalBebidas / 2000)} Pet's 2l de Bebidas</p>`
+
+
+}
+
+function carnePP(duracao) {
+    if (duracao >= 6) {
+        return 650;
+    } else {
+        return 400;
+    }
+}
+
+function cervejaPP(duracao) {
+    if (duracao >= 6) {
+        return 2000;
+    } else {
+        return 1200;
+    }
+}
+function bebidasPP(duracao) {
+    if (duracao >= 6) {
+        return 1500;
+    } else {
+        return 1000;
+    }
+}
